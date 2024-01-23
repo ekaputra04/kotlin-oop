68e783ef6b54274727facadd83eb8dd2366fd728
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-01-23 13:17:20 +0800

    initializer block

--- src/data/Car.kt
+++ src/data/Car.kt
@@ -1,6 +1,11 @@
 package data
 
 class Car(paramName: String, paramBrand: String, paramYear: Int = 2020) {
+
+    init {
+        println("Car $paramName telah dibuat")
+    }
+
     var name: String = paramName
     var brand: String = paramBrand
     var year: Int = paramYear

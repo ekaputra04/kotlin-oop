895a707143646f81614b1364fb8b15cb7c51bf07
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-01-23 13:56:13 +0800

    function overloading

---
+++ src/app/FunctionOverloading.kt
@@ -0,0 +1,13 @@
+package app
+
+import data.Person
+
+fun main() {
+    val eka = Person()
+    eka.firstName = "Eka"
+    eka.middleName = "Putra"
+    eka.lastName = "Juniawan"
+
+    eka.sayHello("Dwi")
+    eka.sayHello("Dwi", "Putri")
+}
--- src/data/Person.kt
+++ src/data/Person.kt
@@ -9,6 +9,10 @@ class Person {
         println("Hello $name, my name is $firstName")
     }
 
+    fun sayHello(firstNameParam: String, lastNameParam: String) {
+        println("Hello $firstNameParam $lastNameParam, my name is $firstName")
+    }
+
     fun run() {
         println("I'm running")
     }

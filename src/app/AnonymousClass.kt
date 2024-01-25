1ab4fa21fedba0a1c4c0144bd5c66fa14b9f3284
ekaputra04 <ekaputrajuniawan@gmail.com>
2024-01-25 13:01:11 +0800

    anonymous class

---
+++ src/app/AnonymousClass.kt
@@ -0,0 +1,29 @@
+package app
+
+import data.Action
+
+fun fireAction(action: Action){
+    action.action()
+}
+
+class SampleAction:Action{
+    override fun action() {
+        println("Sample Action")
+    }
+}
+
+fun main() {
+    fireAction(SampleAction())
+
+    fireAction(object :Action{
+        override fun action() {
+            println("Action one")
+        }
+    })
+
+    fireAction(object :Action{
+        override fun action() {
+            println("Action two")
+        }
+    })
+}
